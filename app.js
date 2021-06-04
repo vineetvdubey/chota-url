bcc22035ed3273baf0a8971190e06f7990776aaa
vineet.dubey <vineet.dubey@zscaler.com>
2021-06-04 10:42:33 +0530

    Added linting configuration

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const router = express.Router();
 const port = process.env.PORT || 3000;
 
-const urls = require("./urls/urls");
-
-app.use(express.static("public"));
-
-app.use("/urls", urls);
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/urls', require('./urls/urls'));
 
 app.listen(port, () => {
   console.log(`Server listening at port: ${port}`);
-  console.log("Started chota-url service");
+  console.log('Started chota-url service');
 });
--- urls/urls.js
+++ urls/urls.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 router.get('/', function (req, res) {
   res.send('urls home page');
-})
+});
 
 module.exports = router;
