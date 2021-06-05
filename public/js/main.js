a1f561b34f854beb80a230afe9817271ed74afd7
vineet.dubey <vineet.dubey@zscaler.com>
2021-06-05 18:52:44 +0530

    revamed ui

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,21 @@ window.onload = () => {
   const longUrlInput = document.querySelector('#long-url');
   const shortenUrlButton = document.querySelector('#shorten');
   const shortUrlOutput = document.querySelector('#short-url');
+  const resultDiv = document.querySelector('#result');
+  let allowCopyResult = false;
 
-  const shortenUrl = (longUrl) => {
+  const isUrlValid = (url) => {
+    const tempUrl = url.trim();
+    const nonEmpty = tempUrl.length > 0;
+    const containsSpace = tempUrl.includes(' ');
+    const containsPeriod = tempUrl.includes('.');
+    return nonEmpty && containsPeriod && !containsSpace;
+  };
+
+  /**
+   * @returns Promise
+   */
+  const postNewUrl = (longUrl) =>
     fetch(`${apiHost}/urls`, {
       method: 'POST',
       headers: {
@@ -15,16 +28,60 @@ window.onload = () => {
       body: JSON.stringify({
         longUrl: longUrl,
       }),
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        shortUrlOutput.innerHTML = json.shortUrl;
-        shortenUrlButton.disabled = false;
-      });
+    });
+
+  const updateResult = (msg) => {
+    shortUrlOutput.value = msg;
+    resultDiv.classList.remove('hidden');
   };
 
-  longUrlInput.addEventListener('keyup', (event, x) => {
-    shortUrlOutput.innerHTML = '';
+  const hideResult = () => {
+    disableCopy();
+    shortUrlOutput.value = '';
+    resultDiv.classList.add('hidden');
+  };
+
+  const enableCopy = () => {
+    allowCopyResult = true;
+  };
+
+  const disableCopy = () => {
+    allowCopyResult = false;
+  };
+
+  const showCopiedMessage = () => {
+    const copiedAlert = document.querySelector('.copy-style');
+    copiedAlert.innerHTML = 'Copied!';
+    copiedAlert.style.display = 'block';
+    setTimeout(() => {
+      copiedAlert.innerHTML = '';
+      copiedAlert.style.display = 'none';
+    }, 1500);
+  };
+
+  const shortenUrl = (longUrl) => {
+    shortenUrlButton.disabled = true;
+    if (isUrlValid(longUrl)) {
+      let longUrlTemp = longUrl;
+      if (!longUrlTemp.startsWith('http')) {
+        longUrlTemp = `https://${longUrlTemp}`;
+      }
+      postNewUrl(longUrlTemp)
+        .then((res) => res.json())
+        .then((json) => {
+          updateResult(json.shortUrl);
+          enableCopy();
+          shortenUrlButton.disabled = false;
+        })
+        .catch(() => updateResult('Unexpected Error - Please try again later.'));
+    } else {
+      updateResult('Invalid URL. Please try again.');
+      shortenUrlButton.disabled = false;
+    }
+  };
+
+  longUrlInput.addEventListener('keyup', (event) => {
+    hideResult();
     if (event.target.value.length > 0) {
       shortenUrlButton.disabled = false;
     } else {
@@ -33,11 +90,14 @@ window.onload = () => {
   });
 
   shortenUrlButton.addEventListener('click', () => {
-    shortenUrlButton.disabled = true;
-    let inputLongUrl = longUrlInput.value;
-    if (!inputLongUrl.startsWith('http')) {
-      inputLongUrl = `https://${inputLongUrl}`;
+    shortenUrl(longUrlInput.value);
+  });
+
+  resultDiv.addEventListener('click', () => {
+    if (allowCopyResult) {
+      shortUrlOutput.select();
+      document.execCommand('copy');
+      showCopiedMessage();
     }
-    shortenUrl(inputLongUrl);
   });
 };
